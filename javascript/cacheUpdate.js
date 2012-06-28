1cf032a8a510979feb3b116850009f23e54843cd
archcooler <mb@travailassocie.com>
2012-06-28 11:06:44 +0200

    v2

--- javascript/cacheUpdate.js
+++
@@ -1,1 +0,0 @@
-var webappCache = window.applicationCache;function updateCache(){webappCache.swapCache();alert("Une nouvelle version est disponible.\nVeuillez recharger l'application pour l'actualiser.");}if(navigator.onLine == true){webappCache.update();	webappCache.addEventListener("updateready", updateCache, true);}
